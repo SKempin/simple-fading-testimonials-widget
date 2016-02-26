9f09c86a7dce34bce65fcc861995167022775341
Stephen Kempin <info@stephenkempin.co.uk>
2016-02-26 11:24:48 +0000

    Adding minified JS

--- public/sft-public.php
+++ public/sft-public.php
@@ -96,7 +96,7 @@ class Simple_Fading_Testimonials_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/sft-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/sft-public.min.js', array( 'jquery' ), $this->version, false );
 
 	}
 	
